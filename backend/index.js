edbc2892c00d0ff14ad4212ebdd7aa9db3ef1a8a
skn0012 <122576334+skn0012@users.noreply.github.com>
2025-04-19 18:01:49 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -45,8 +45,8 @@ function flipCard() {
     let i = Math.floor(Math.random() * 53) + 1;
     //let frontOfCard = document.getElementById('frontCard');
     //frontOfCard.src = `assets/cards/${i}.png`;
-    const fcard = document.getElementById('cardss).src; 
-    if (fcard != "cardBack.png") {
+    const fcard = document.getElementById('cardss').src; 
+    if (fcard == "cardBack.png") {
         document.getElementById('cardss').src = `./assets/cards/${i}.png`;
     }
     document.getElementById('flipCard').classList.toggle('flip');
